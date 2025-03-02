d6a461a9959ff17155a24d0eb43ad8157dd3c89a
Adekolaomooba <adekola.adebayo2@gmail.com>
2025-03-02 20:07:20 +0100

    Modified Services Commit

--- src/components/StylishList.jsx
+++ src/components/StylishList.jsx
@@ -1,28 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function StylishList({bground, number, title, desc, arrow, rotateAmount}) {
-  return (
-    <div className= {`w-full h-[150px] sm:h-[100px]  sm:flex text-white px-5  relative
-                    transition-all duration-700 ease-in-out`}
-        style={{backgroundColor: `${bground}`}}>
-            <div className=' flex  w-full sm:w-[40%] '>
-              <p className=' text-2xl font-bold w-[10%] my-auto '>
-                {number}
-              </p>
-              <h3 className=' text-2xl md:text-3xl lg:text-4xl font-bold w-[90%] my-auto capitalize '>
-                {title}
-              </h3>
-            </div>
-            <div className=' flex w-full sm:w-[60%] '>
-              <p className=' w-[90%] my-auto first-letter:uppercase '>
-                {desc}
-              </p>
-              <div className=' w-[10%] flex justify-end '>
-              <i className={`pi ${arrow} text-2xl my-auto w-fit 
-                    transition-all duration-700 ease-in-out`}
-                style={{ transform: `rotate(${rotateAmount}deg)` }}></i>
-              </div>
-            </div>
-    </div>
-  )
+export default function StylishList({ arr = [], arrow }) {
+
+    const [bgState, setBgState] = useState(null)
+    const [translateValue, stTranslateValue] = useState(0)
+
+    return (
+        <ul className=' px-2 sm:px-3 md:px-4 '>
+            {
+                arr.map((ser, index) => {
+
+                    const isHovered = bgState == index
+
+                    return (
+                        <li key={index}
+                            onMouseEnter={() => { setBgState(index); stTranslateValue(index) }}
+                            onMouseLeave={() => setBgState(null)}
+                            className=' cursor-pointer w-full h-fit border-[#44163c] border-b-[1px] m-0 relative py-3 '>
+
+                            <div className={`${index !== 0 && ('hidden')} w-full h-full
+                                    bg-linear-to-r from-[#a1368f] via-[#a1368f] 
+                                    absolute top-0 transition-all duration-500 ease-in-out `}
+                                style={{ transform: `translateY(${(translateValue * 100) + translateValue}%)` }}>
+                            </div>
+
+                            <div className={`w-full h-[150px] sm:h-[100px]  sm:flex text-white px-5  relative
+                                     transition-all duration-700 ease-in-out`}>
+                                <div className=' flex  w-full sm:w-[40%] '>
+                                    <p className=' text-2xl font-bold w-[10%] my-auto '>
+                                        {ser.num}
+                                    </p>
+                                    <h3 className=' text-2xl md:text-3xl lg:text-4xl font-bold w-[90%] my-auto capitalize '>
+                                        {ser.title}
+                                    </h3>
+                                </div>
+                                <div className=' flex w-full sm:w-[60%] '>
+                                    <p className=' w-[90%] my-auto first-letter:uppercase '>
+                                        {ser.description}
+                                    </p>
+                                    <div className=' w-[10%] flex justify-end '>
+                                        <i className={`pi ${arrow} text-2xl my-auto w-fit 
+                                                transition-all duration-700 ease-in-out`}
+                                            style={{ transform: `rotate(${isHovered ? -90 : 0}deg)` }}></i>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </li>
+                    )
+                })
+            }
+        </ul>
+    )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 import HeaderAndDescription from '../components/HeaderAndDescription'
 import services from '../assets/utils/servicesUtils/services.json'
 import StylishList from '../components/StylishList'
 
 export default function Home() {
-
-  const [bgState, setBgState] = useState(null)
-  const [translateValue, stTranslateValue] = useState(0)
 
   return (
     <div>
@@ -18,32 +15,9 @@ export default function Home() {
               desc={`We put your ideas and thus your wishes in the form of a unique web project that inspires you and you customers.`} />
           </div>
 
-          <ul className=' px-2 sm:px-3 md:px-4 '>
-            {
-              services.map((ser, index)=>{
-
-                const isHovered = bgState == index
-
-                return(
-                  <li key={index}
-                      onMouseEnter={()=>{setBgState(index); stTranslateValue(index)}}
-                      onMouseLeave={()=>setBgState(null)}
-                      className=' cursor-pointer w-full h-fit border-[#44163c] border-b-[1px] m-0 relative py-3 '>
-                    
-                    <div className={`${index !== 0 && ('hidden')} w-full h-full
-                            bg-linear-to-r from-[#a1368f] via-[#a1368f] 
-                            absolute top-0 transition-all duration-500 ease-in-out `}
-                        style={{transform: `translateY(${(translateValue*100)+translateValue}%)`}}></div>
-                        
-                    <StylishList number={ser.num} title={ser.title} desc={ser.description} 
-                      arrow={'pi-arrow-down-right'}
-                      rotateAmount={isHovered ? -90 : 0} />
-                      
-                  </li>
-                )
-              })
-            }
-          </ul>
+          <div>
+            <StylishList arr={services} arrow={'pi-arrow-down-right'} />
+          </div>
         </div>
 
       </div>
