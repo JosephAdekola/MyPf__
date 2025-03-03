ff80cfb7e958501704557e69ab6b1ac43b133706
Adekolaomooba <adekola.adebayo2@gmail.com>
2025-03-03 12:03:30 +0100

    reentWorkAgain

--- src/assets/utils/recentWorkUtils/recentWorksData.jsx
+++ src/assets/utils/recentWorkUtils/recentWorksData.jsx
@@ -1,10 +1,10 @@
 import rushrushNgShort from '../../../assets/rushrushNgShot.jpg'
 
  const recentWorksData = [
-    { id: 1, image: rushrushNgShort, name: "rushrushNG", name: "project was about precision and information", arrow: "pi pi-arrow-up-right" },
-    { id: 2, image: rushrushNgShort, name: "rushrushNG", name: "project was about precision and information", arrow: "pi pi-arrow-up-right" },
-    { id: 3, image: rushrushNgShort, name: "rushrushNG", name: "project was about precision and information", arrow: "pi pi-arrow-up-right" },
-    { id: 4, image: rushrushNgShort, name: "rushrushNG", name: "project was about precision and information", arrow: "pi pi-arrow-up-right" }
+    { id: 1, image: rushrushNgShort, name: "rushrushNG", description: "project was about precision and information", arrow: "pi pi-arrow-up-right" },
+    { id: 2, image: rushrushNgShort, name: "rushrushNG", description: "project was about precision and information", arrow: "pi pi-arrow-up-right" },
+    { id: 3, image: rushrushNgShort, name: "rushrushNG", description: "project was about precision and information", arrow: "pi pi-arrow-up-right" },
+    { id: 4, image: rushrushNgShort, name: "rushrushNG", description: "project was about precision and information", arrow: "pi pi-arrow-up-right" }
 ]
 
 export default recentWorksData;
